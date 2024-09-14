fa766be468852c301b9a0fa648898214d2c2a7dd
SantiP11 <90872232+SantiP11@users.noreply.github.com>
2024-09-14 20:00:26 -0300

    Create pxt.blocks.ts
    Example block code

---
+++ pxt.blocks.ts
@@ -0,0 +1,17 @@
+// pxt.blocks.ts
+// This file defines the blocks for the extension
+pxt.blocks.registerBlock("led.on", {
+  name: "Turn LED On",
+  args: {},
+  category: "LED",
+  color: "#00ff00", 
+  icon: "LED"
+});
+
+pxt.blocks.registerBlock("led.off", {
+  name: "Turn LED Off",
+  args: {},
+  category: "LED",
+  color: "#ff0000", 
+  icon: "LED_OFF"
+});

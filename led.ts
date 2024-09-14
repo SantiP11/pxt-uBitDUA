d2afee5f8896ebb09de564fb47b0b4d78212e73b
SantiP11 <90872232+SantiP11@users.noreply.github.com>
2024-09-14 19:59:44 -0300

    Create led.ts
    example of ts file

---
+++ led.ts
@@ -0,0 +1,14 @@
+// led.ts
+namespace led {
+  // Function to turn the LED on
+  export function on() {
+    // Replace this with the actual code to turn on the LED
+    console.log("LED ON!"); 
+  }
+
+  // Function to turn the LED off
+  export function off() {
+    // Replace this with the actual code to turn off the LED
+    console.log("LED OFF!"); 
+  }
+}
